6189fe03212f0558bce204f5c916ecf8a773e620
adminproconty <christian.teran@proconty.com>
2018-04-19 10:54:34 -0500

    Inicio Branch Master
    Incio Branch Master

---
+++ model/Conexion.php
@@ -0,0 +1,8 @@
+<?php
+
+	$conexion = new mysqli("localhost", "root", "", "proco389_sginewsky");
+	
+	if (mysqli_connect_errno()) {
+	    printf("Connect failed: %s\n", mysqli_connect_error());
+	    exit();
+	}
